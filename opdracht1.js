dfc1fad6a3804f37e8639693144a0c7ce2807f70
LeviHoevers <99065078@mydavinci.nl>
2020-10-30 10:47:34 +0100

    opdracht 2 gepushed

---
+++ opdracht1.js
@@ -0,0 +1,11 @@
+function HelloWorld (qty){
+	for (i=1; i<=qty; i++) {
+		document.write(i + ". Hello World <br>");
+	}
+}
+HelloWorld(3);
+
+document.write("<br>")
+
+HelloWorld(7);
+
---
+++ opdracht2.js
@@ -0,0 +1,26 @@
+function plus(num1, num2){
+var calculate= (num1 + num2);
+document.write (num1 + " + " + num2 + " = " + calculate + "<br>")
+}
+
+function min(num1, num2){
+var calculate= (num1 - num2);
+document.write (num1 + " + " + num2 + " = " + calculate + "<br>")
+}
+
+function multiply(num1, num2){
+var calculate= (num1 * num2);
+document.write (num1 + " + " + num2 + " = " + calculate + "<br>")
+}
+
+function divide(num1, num2){
+var calculate= (num1 / num2);
+document.write (num1 + " + " + num2 + " = " + calculate + "<br>")
+}
+
+plus(10, 12);
+min(58, 34)
+multiply(6, 7)
+divide(144, 12)
+plus(12,1)
+min(34,1)
